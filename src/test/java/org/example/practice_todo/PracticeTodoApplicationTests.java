1c9422cc69b5c20587cef42f5a6c823b9c4fd7ec
star-pooh <eunki92@gmail.com>
2024-12-06 14:09:48 +0900

    Init : Create practice_todo project 일정관리 과제 프로젝트 생성

---
+++ src/test/java/org/example/practice_todo/PracticeTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.practice_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeTodoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
